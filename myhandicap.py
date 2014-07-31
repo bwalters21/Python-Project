740ed0027c78a6f3849822feb0763191be5f4cc2
unknown <bpwalt9934@GIS-VLAB1.ad.uauth.net>
2014-07-31 17:21:41 -0400

    This is the Python Shell after calculating my most recent five rounds of  golf

---
+++ myhandicap.py
@@ -0,0 +1,21 @@
+Python 2.7.2 (default, Jun 12 2011, 15:08:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+enter the score of your round: 83
+enter the course rating: 71
+enter the slope number: 134
+enter the score of your round: 74
+enter the course rating: 69.9
+enter the slope number: 124
+enter the score of your round: 75
+enter the course rating: 71.2
+enter the slope number: 137
+enter the score of your round: 82
+enter the course rating: 71.2
+enter the slope number: 137
+enter the score of your round: 79
+enter the course rating: 71
+enter the slope number: 131
+Your Golf Handicap: 3.0
+>>> 
